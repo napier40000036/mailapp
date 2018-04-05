a26dde40de74467bb3ab3c389a4ecc1b53ea551d
cs66 <ajcumming@gmail.com>
2018-04-05 13:20:33 +0630

    shows message body on click

--- server.py
+++ server.py
@@ -15,6 +15,15 @@ def mailList():
         l.append({"id":m["id"],"subject":m["subject"],"from":m["from"]})
     return json.dumps(l)
 
+@app.route("/mail/<id>") #Get a single messages
+def mailItem(id):
+    mm = None
+    ml = json.load(open('inbox.json'))
+    for m in ml:
+        if m['id'] == id:
+        	mm = m
+    return json.dumps(mm)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5036,debug=True)
 
--- static/mail.js
+++ static/mail.js
@@ -7,12 +7,25 @@ $(function(){
 			$('#mail-list').empty();
 			for(var i=0;i<ml.length;i++){
 				var d = $('<div/>',{'class':'mail-summary'});
+				var id = $('<div/>',{text:ml[i].id,'class':'id'});
 				var sender = $('<div/>',{text:ml[i].from,'class':'sender'});
 				var subject = $('<div/>',{text:ml[i].subject,'class':'subject'});
+				d.append(id);
 				d.append(sender);
 				d.append(subject);
 				$('#mail-list').append(d);
+				d.click(function(){
+					var id = $('.id',this).text();
+					$.getJSON('/mail/'+id,function(d){
+						$('#mail-body').text(d.body)
+					});
+				})
 			}
 		});
 	})
+	$('#refresh').trigger('click');
 })
+
+
+
+
