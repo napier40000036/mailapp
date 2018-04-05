440211586a9a8a24cfd26a4caab622a8ecfea210
cs66 <ajcumming@gmail.com>
2018-04-05 11:23:03 +0630

    ajax call works

--- server.py
+++ server.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def indexPage():
 	return render_template('index.html')
 
+@app.route("/mail") #Get a list of messages
+def mailList():
+    l = []
+    ml = json.load(open('inbox.json'))
+    for m in ml:
+        l.append({"id":m["id"],"subject":m["subject"],"from":m["from"]})
+    return json.dumps(l)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5036,debug=True)
+
+
+
+
+
+
+
+
--- static/mail.js
+++ static/mail.js
@@ -4,7 +4,12 @@ $(function(){
 	$('#refresh').click(function(){
 		$('#mail-list').text('Getting mail. Please wait...');
 		$.getJSON('/mail',function(ml){
-			$('#mail-list').text("Mail retrieved :)");
+			$('#mail-list').empty();
+			for(var i=0;i<ml.length;i++){
+				var d = $('<div/>');
+				d.text(ml[i]);
+				$('#mail-list').append(d);
+			}
 		});
 	})
 })
