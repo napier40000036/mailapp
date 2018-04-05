5434b4170b20ee2a6604e11687bf3b914ee48c19
cs66 <ajcumming@gmail.com>
2018-04-05 10:35:39 +0630

    making progress

--- static/mail.js
+++ static/mail.js
@@ -1,4 +1,10 @@
 $(function(){
 	$('#mail-list').text("Andrew was here");
 	$('#mail-body').text("He was here too!");
+	$('#refresh').click(function(){
+		$('#mail-list').text('Getting mail. Please wait...');
+		$.getJSON('/mail',function(ml){
+			$('#mail-list').text("Mail retrieved :)");
+		});
+	})
 })
